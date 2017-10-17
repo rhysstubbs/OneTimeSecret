7958813dc77425169b7d33168a617f4244f9b883
Rhys Stubbs <rhys@soundintheory.co.uk>
2017-10-17 14:48:52 +0100

    404 page create, additional pages disabeld, colours ammended to match the original design.

--- public/web/assets/js/copy.js
+++ public/web/assets/js/copy.js
@@ -1,15 +1,19 @@
-var btnCopy = document.querySelector('#btn-copy');
-btnCopy.addEventListener('click', function (event) {
-    var link = document.querySelector('#secreturi');
-    var range = document.createRange();
-    range.selectNode(link);
-    window.getSelection().addRange(range);
-    try {
-        var successful = document.execCommand('copy');
-        var msg = successful ? 'successful' : 'unsuccessful';
-        console.log('Copy email command was ' + msg);
-    } catch(err) {
-        alert('Oops, try copying manually!');
-    }
-    window.getSelection().removeAllRanges();
-});
+window.onload = function () {
+    var btnCopy = document.querySelector('#btn-copy');
+    btnCopy.addEventListener('click', function (event) {
+        var link = document.querySelector('#secreturi');
+        var range = document.createRange();
+        range.selectNode(link);
+        window.getSelection().addRange(range);
+        try {
+            var successful = document.execCommand('copy');
+            var msg = successful ? 'successful' : 'unsuccessful';
+            console.log('Copy email command was ' + msg);
+        } catch(err) {
+            alert('Oops, try copying manually!');
+        }
+        window.getSelection().removeAllRanges();
+    });
+};
+
+
