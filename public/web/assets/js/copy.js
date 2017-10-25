fd37b33ccbb26b62155fb011009c6c05fde3d520
Rhys Stubbs <rhys@soundintheory.co.uk>
2017-10-25 15:45:43 +0100

    Fix for javascript and minor tweaks to ui

--- public/web/assets/js/copy.js
+++ public/web/assets/js/copy.js
@@ -7,18 +7,15 @@ window.onload = function () {
         window.getSelection().addRange(range);
         try {
             var successful = document.execCommand('copy');
-            var msg = successful ? 'successful' : 'unsuccessful';
-
-            if (msg.equals('successful')) {
-                var label = document.createElement("label");
-                var labelText = document.createTextNode("Copied!");
-                label.appendChild(labelText);
-                document.getElementById('url-form').appendChild(label);
-            }
-
-        } catch(err) {
+        } catch (err) {
             alert('Oops, try copying manually!');
         }
+
+        var label = document.createElement("label");
+        var labelText = document.createTextNode("Copied!");
+        label.appendChild(labelText);
+        document.getElementById('url-form').appendChild(label);
+
         window.getSelection().removeAllRanges();
     });
 };
