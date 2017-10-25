781b477926d612ac209b4e535b0dc918e2c42642
Rhys Stubbs <rhys@soundintheory.co.uk>
2017-10-25 16:14:17 +0100

    Js for copy append is fixed, moved var outside of event scope.

--- public/web/assets/js/copy.js
+++ public/web/assets/js/copy.js
@@ -1,22 +1,26 @@
 window.onload = function () {
     var btnCopy = document.querySelector('#btn-copy');
+    var copied = 0;
+
     btnCopy.addEventListener('click', function (event) {
         var link = document.querySelector('#secreturi');
         var range = document.createRange();
         range.selectNode(link);
         window.getSelection().addRange(range);
+
         try {
             var successful = document.execCommand('copy');
+            copied++;
+            if (copied == 1) {
+                var label = document.createElement("label");
+                var labelText = document.createTextNode("Copied!");
+                label.appendChild(labelText);
+                document.getElementById('url-form').appendChild(label);
+            }
+            window.getSelection().removeAllRanges();
         } catch (err) {
             alert('Oops, try copying manually!');
         }
-
-        var label = document.createElement("label");
-        var labelText = document.createTextNode("Copied!");
-        label.appendChild(labelText);
-        document.getElementById('url-form').appendChild(label);
-
-        window.getSelection().removeAllRanges();
     });
 };
 
