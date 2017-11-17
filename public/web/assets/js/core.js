197164ec084b5082427fda46018802db434afc38
Rhys Stubbs <rhys@soundintheory.co.uk>
2017-11-17 20:47:49 +0000

    Upgrade to WebPack

--- public/web/assets/js/core.js
+++ public/web/assets/js/core.js
@@ -1,8 +1,27 @@
+/**
+ * Imports for Sass styling.
+ * Minified version -> assets/dist/app.min.css
+ */
+require('../scss/screen.scss');
+require('../../../../node_modules/font-awesome/scss/font-awesome.scss');
+
+/**
+ * Core Javascript/Jquery functionality
+ * Minified version -> assets/dist/app.bundle.min.js
+ */
+
+require('../../../../node_modules/clipboard/dist/clipboard.js');
+require('../../../../node_modules/jquery-placeholder/jquery.placeholder.js');
+require('./lib/main.js');
+require('./lib/plugins.js');
+require('./lib/passgen-template.js');
+
+
 (function ($) {
 
-    $(document).ready(function () {
+    $(document).on('ready', function () {
 
-       window.page = new Page();
+        //window.page = new Page();
 
         var copied = 0;
 
@@ -57,83 +76,12 @@
                 }
             }
         });
-
     });
 
-    // ----------------------------------------------------------------------------------------------------
+    // ------------------------------------------------------------------------------------------------------
     // COMMON PAGE FUNCIONALITY
     Page = function () {
-        var heightGroups = new EqualHeightGroups();
-    };
 
-    // ------------------------------------------------------------------------------------------------------
-    // EQUAL HEIGHT GROUPS
-    EqualHeightGroups = function (wrap, opts) {
-        this.opts = $.extend({}, EqualHeightGroups.defaults, opts || {});
-        this.wrap = wrap || $('body');
-
-        if (!this.wrap.find('[data-height-group]').length) return;
-
-        var groups = {};
-
-        this.wrap.find('[data-height-group]').each(function () {
-
-            var groupId = $(this).attr('data-height-group');
-            if (groups.hasOwnProperty(groupId)) return;
-
-            groups[groupId] = $('[data-height-group="' + groupId + '"]');
-
-        });
-
-        this.groups = groups;
-
-        $(window).on('load resize', $.proxy(this.onResize, this));
-        this.onResize();
-
-    }
-
-    EqualHeightGroups.defaults = {};
-
-    EqualHeightGroups.prototype.processGroup = function (groupId) {
-        var group = this.groups[groupId].height('auto'),
-            lines = {};
-
-        group.each(function (i, el) {
-            var cPos = $(el).offset().top + '';
-            if (!lines.hasOwnProperty(cPos)) lines[cPos] = [];
-            lines[cPos].push(el);
-        });
-
-        for (var p in lines) {
-            var line = $(lines[p]).map(function () {
-                return $(this).toArray();
-            });
-            var maxHeight = Math.max.apply(null, line.map(function () {
-                return $(this).height();
-            }).get());
-            line.height(maxHeight);
-        }
-    }
-
-    EqualHeightGroups.prototype.onResize = function () {
-        if (this.throttleResize == true) {
-            this.resizeAttempts++;
-            return;
-        }
-
-        for (var p in this.groups) {
-            this.processGroup(p);
-        }
-
-        this.throttleResize = true;
-        this.resizeAttempts = 0;
-        clearTimeout(this.resizeTimeout || null);
-        setTimeout($.proxy(function () {
-            this.throttleResize = false;
-            if (this.resizeAttempts > 0) {
-                this.onResize();
-            }
-        }, this), 500);
     }
 
 })(jQuery);
